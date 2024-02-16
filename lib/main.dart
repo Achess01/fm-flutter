be6102e3ff1907ea6a9d1376c975b295a84cd2c7
Achess <jealtzo26@gmail.com>
2024-02-16 17:00:00 -0600

    Adding assets

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coffee Masters',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.brown
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class Greet extends StatefulWidget {
+  const Greet({super.key});
+
+  @override
+  State<Greet> createState() => _GreetState();
+}
+
+class _GreetState extends State<Greet> {
+  // State variable
+
+  var name = "";
+  @override
+  Widget build(BuildContext context) {
+    var greetStyle = const TextStyle(fontSize: 24);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Text(
+                "Hello $name",
+                style: greetStyle,
+              ),
+              Text(
+                "!!!",
+                style: greetStyle,
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 24.0, left: 24),
+          child: TextField(
+            onChanged: (value) => setState(() {
+              name = value;
+            }),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -30,9 +73,7 @@ class HelloWorld extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -43,10 +84,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Image.asset("images/logo.png"),
       ),
-      body: const HelloWorld(),
+      body: const Greet(),
     );
   }
 }
